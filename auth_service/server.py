fbc48502428be85890be4caab81badd79ede2571
Mohamed Karam <mohamed.karam@id3m.com>
2025-06-07 20:04:12 +0300

    Write gatway service to upload file and check auth

--- auth_service/server.py
+++ auth_service/server.py
@@ -4,43 +4,45 @@ from flask_mysqldb import MySQL
 
 
 server = Flask(__name__)
-mysql = MySQL(server)
 server.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 server.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 server.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
 server.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 server.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+mysql = MySQL(server)
 
 @server.route('/login', methods=['POST'])
 def login():
-    auth = request.authorization
+    print("Headers:", request.headers)
+    print("Content-Type:", request.content_type)
+    print("Raw data:", request.data)
 
-    if not auth:
-        return 'missing credentials', 401
-    
-    # check db for username and passowrd 
+    data = request.get_json()
+    print("Parsed JSON:", data)
+
+    print(data)
+    if not data or 'email' not in data or 'password' not in data:
+        return data, 'Missing credentials', 401
+
+    email = data['email']
+    password = data['password']
+
+    print(email, password)
+    # check db for email and password  
     cur = mysql.connection.cursor()
-    res = cur.excute(
-        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
-    )
+    cur.execute("SELECT email, password FROM user WHERE email=%s", (email,))
+    user_row = cur.fetchone()
 
-    if res > 0:
-        user_row = cur.fetchone()
-        email = user_row[0]
-        password = user_row[1]
-
-        if auth.username != email or auth.password != password:
-            return 'invalid credentials', 401
-        else:
-            return createJWT(auth.username, os.environ.get('JWT_SECRET'), True)
+    if user_row and user_row[1] == password:
+        return createJWT(email, os.environ.get('JWT_SECRET'), True)
     else:
-        return 'invalid credentials', 401
+        return 'Invalid credentials', 401
 
-def createJWT(username, secret, auth):
+def createJWT(email, secret, auth):
     return jwt.encode(
         {
-            'username': username,
-            'exp': datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(bays=1),
+            'email': email,
+            'exp': datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
             'iat': datetime.datetime.utcnow(),
             'admin': auth
         },
