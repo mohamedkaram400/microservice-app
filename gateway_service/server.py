b44e248a3c22bd343772f9f2e8160976936c0e09
Mohamed Karam <mohamed.karam@id3m.com>
2025-06-10 20:57:09 +0300

    Write download method for dowload mp3 in gateway service

--- gateway_service/server.py
+++ gateway_service/server.py
@@ -9,9 +9,10 @@ from bson.objectid import ObjectId
 server = Flask(__name__)
 
 mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
-# mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
 
-fs = gridfs.GridFS(mongo_video.db)
+videos_fs = gridfs.GridFS(mongo_video.db)
+mp3s_fs = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
@@ -29,6 +30,9 @@ def login():
 def upload():
     access, err = validate.token(request)
 
+    if err:
+        return err
+    
     access = json.load(access)
 
     if access['admin']:
@@ -36,7 +40,7 @@ def upload():
             return "exctly 1 file required", 400
         
         for _, f in request.files.items():
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, videos_fs, channel, access)
             if err:
                 return err
         return 'success', 200
@@ -46,7 +50,26 @@ def upload():
 
 @server.route('/download', methods=['GET'])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+    
+    access = json.load(access)
+
+    if access['admin']:
+        fid_string = request.args.get('fid')
+        if not fid_string:
+            return 'fid is required', 400
+        
+        try:
+            out = mp3s_fs.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+    
+    return 'not authorized', 401
 
 
 if __name__ == "__main__":
